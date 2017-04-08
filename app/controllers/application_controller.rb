ba4977fe8e144739222372456ed8094fa413c310
Timothy Smith <smith.timothyh@gmail.com>
2017-04-07 18:31:33 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def edit
-    user_id = params[:id]
-    @user = Location.find_by_id(user_id)
-  end
 end
