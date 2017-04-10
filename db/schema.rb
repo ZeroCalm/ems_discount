2f9bd80b2d8a75c3c9d0fb762624892c81684986
zeroCalm <smith.timothyh@gmail.com>
2017-04-09 19:21:12 -0700

    itsWorkingForNow

--- db/schema.rb
+++ db/schema.rb
@@ -52,18 +52,6 @@ ActiveRecord::Schema.define(version: 20170409173350) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "type"
-    t.string   "address"
-    t.text     "description"
-    t.string   "discount_offered"
-    t.string   "latitude"
-    t.string   "longitude"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
